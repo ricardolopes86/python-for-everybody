9c46b43d8c93eb13d61e999594454b5792d53c68
ricardolopes86 <ricardolopes18@yahoo.com.br>
2016-04-12 23:05:22 +0200

    Corrigido o Assignment 9.4

--- assignment_9.4.py
+++ assignment_9.4.py
@@ -1,17 +1,18 @@
 name = raw_input("Nome do arquivo:")
 handle = open(name,'r')
-texto = handle.read()
-palavras = texto.split()
 
-contador = {}
-for word in palavras:
-	contador[word] = contador.get(word,0) + 1
+contador = dict()
+for line in handle:
+	if line.startswith("From"):
+		palavras = line.split()
+		for palavra in palavras:
+			if '@' in palavra:
+				contador[palavra] = contador.get(palavra,0) + 1
 
 big_count = None
 big_word = None
 for word,count in contador.items():
-	if '@' in word:
-		if big_count is None or count > big_count:
-			big_word = word
-			big_count = count
+	if big_count is None or count > big_count:
+		big_word = word
+		big_count = count
 print big_word,big_count
